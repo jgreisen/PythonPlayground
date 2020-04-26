7465d372990d623f67dc75d9aa6d98bdcb6495c9
jgreisen <greisenj@gmail.com>
2020-04-25 23:02:04 -0700

    Test

---
+++ MovieDB.py
@@ -0,0 +1,31 @@
+class Movie:
+  def __init__(self, title, rating):
+    self.title = title
+    self.rating = rating
+
+movieDB = []
+
+def addJaws(Movie):
+  movieDB.append(Movie)
+
+def printMovies(list):
+  for x, movie in enumerate(list):
+    print('The movie at index {} in our movie database is named {} and has an {} rating.'.format(str(x), movie.title, movie.rating))
+
+
+print('Adding John Wick to our movie database via append...')
+movieDB.append(Movie("John Wick", "R"))
+printMovies(movieDB)
+
+print('Adding Jaws to our movie database via function...')
+addJaws(Movie("Jaws", "R"))
+printMovies(movieDB)
+
+print('Add a new movie to our database!')
+print('Please enter a movie title:')
+movieName = input()
+print('Please enter a movie rating:')
+movieRating = input()
+
+movieDB.append(Movie(movieName, movieRating))
+printMovies(movieDB)
